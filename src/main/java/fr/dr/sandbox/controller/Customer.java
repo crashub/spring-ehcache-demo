acd3570ddb1f13f12ea51e3f392a2b7efc3738b7
Rieu Damien <damien.rieu@gmail.com>
2012-06-02 00:43:23 +0200

    Add 1 customer in cache get it and clear cache.

---
+++ src/main/java/fr/dr/sandbox/controller/Customer.java
@@ -0,0 +1,25 @@
+package fr.dr.sandbox.controller;
+
+public class Customer {
+	public String address;
+    public String name;
+    public String id;
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+}
---
+++ src/main/java/fr/dr/sandbox/controller/CustomerService.java
@@ -0,0 +1,52 @@
+package fr.dr.sandbox.controller;
+
+import java.util.List;
+
+import net.sf.ehcache.Cache;
+import net.sf.ehcache.CacheManager;
+import net.sf.ehcache.Element;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import fr.dr.sandbox.controller.Customer;
+import com.googlecode.ehcache.annotations.Cacheable;
+import com.googlecode.ehcache.annotations.TriggersRemove;
+import com.googlecode.ehcache.annotations.When;
+
+@Service("CustomerService")
+public class CustomerService {
+        @Autowired
+        public CacheManager cacheManager;
+        
+        public boolean saveCustomer(Customer customer){
+                Cache cache=cacheManager.getCache("customer");
+                cache.put(new Element(customer.getId(),customer));
+                return true;
+        }
+        
+        public Customer getCachedCustomer(String customerId){
+                Cache cache=cacheManager.getCache("customer");
+                return (Customer)cache.get(customerId).getObjectValue();        
+        }
+        
+        @Cacheable(cacheName="customer")
+        public Customer getCustomer(String customerId){
+                Customer c=new Customer();
+                c.setId(customerId);
+                c.setName("Adeel Shafqat");
+                c.setAddress("Address");
+                return c;
+        }
+        
+        @TriggersRemove(cacheName = "customer", when = When.AFTER_METHOD_INVOCATION, removeAll = true) 
+        public boolean clearCache(){
+                Cache cache=cacheManager.getCache("customer");
+                List l=cache.getKeys();
+                for(int i=0;i<l.size();i++){
+                        System.out.println(l.get(i));
+                }
+                return true;
+        }
+}
+
